72d80cc37c28f837fd7e8bc7b3b8ced97dd50fa2
max-student <max-student@maxtrain.com>
2020-03-13 14:23:47 -0400

    jQuery

--- js/index.js
+++ js/index.js
@@ -57,3 +57,30 @@ const convertf = () =>{
     let c = 5/9 * (f - 32);
     document.getElementById("Celsiusans").innerText = c;
 }
+const f = "Fizz";
+const b = "Buzz";
+const fb = f+b;
+
+const fizzbuzz = () => {
+    for(let idx = 0; idx <= 100; idx++){
+        if(idx %3 ==0 && idx %5 == 0){
+            out(fb);
+            continue;
+        }if(idx %3 == 0){
+            out(f);
+            continue;
+        }if(idx %5 == 0){
+            out(b);
+            continue;
+        }out(idx);
+    }
+
+}
+const out = (output) => {
+    let pCtrl = document.getElementById("out");
+    let color = "black";
+    if(output == fb){color = "purple";}
+    if(output == f){color = "red";}
+    if(output == b){color = "blue";}
+    pCtrl.innerHTML += `<span style="color:${color};"> ${output}</span>,`;
+}
